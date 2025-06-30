eb975745f38d3dc4a36d3bd6a27bbdea8afdcafb
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-06-30 11:28:03 +0300

    feat: implement controlled SearchForm with input and submit handling

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,10 +1,45 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { type FormEvent } from 'react';
+import { useState } from 'react';
+
 type SearchFormProps = {
   userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
-  return <div>SearchForm</div>;
+  const [text, setText] = useState(userName);
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text === '') {
+      console.log('Please enter a username');
+      return;
+    }
+    setUserName(text);
+  };
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className='flex items-center gap-x-2 w-full lg:w-1/3 mb-8'
+    >
+      <Label htmlFor='search' className='sr-only'>
+        Search
+      </Label>
+      <Input
+        type='text'
+        id='search'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder='Search Github User...'
+        className='flex-grow bg-background'
+      />
+      <Button type='submit'>Search</Button>
+    </form>
+  );
 };
 
 export default SearchForm;
